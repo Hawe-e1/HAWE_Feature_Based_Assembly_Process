55e1dd8349a13c38fca257eed4109f70397b4c1d
Benjamin Baffy <BenjaminBaffy@gmail.com>
2022-08-01 17:48:17 +0200

    format and small bug fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import procgen
 if __name__ == "__main__":
     type_codes = procgen.utils.load_typcodes_from_file_path("data/example_type_codes.txt")
     #print(type_codes[0])
-    feature_model = utils.load_json_from_file_path("data/small_feature_model.json")
+    feature_model = procgen.utils.load_json_from_file_path("data/feature_model.json")
     fm = procgen.feature_model.FeatureModel(feature_model)
     print(fm.check_fm_satisfiability())
     #for tc in type_codes:
--- procgen/feature_model.py
+++ procgen/feature_model.py
@@ -1,6 +1,8 @@
+from typing import Dict, List
 import procgen.product as product
 import procgen.specification as specification
 import procgen.utils as utils
+from procgen.feature_model_type import FeatureModelType
 
 import re
 import sympy
@@ -8,16 +10,21 @@ import sympy.abc
 import sympy.logic.boolalg as boolalg
 import sympy.logic.inference as inference
 
+
 class FeatureModel:
     def __init__(self, feature_model):
         self.all_symbols = []
-        
+
         self.structure = feature_model["structure"]
-        self.constraints = feature_model["constraints"]
-        self.order_constraints = feature_model["order_constraints"]
-        self.assembly_steps = feature_model["assembly_steps"]
+        self.constraints: List[List[str]] = feature_model["constraints"]
+        self.order_constraints: List[
+            Dict[str, List[int] | List[str | Dict[str, str]]]
+        ] = feature_model["order_constraints"]
+        self.assembly_steps: Dict[str, List[str]] = feature_model["assembly_steps"]
         root = list(feature_model["structure"].keys())
-        assert len(root) == 1, "There is more than one root of the feature model which makes it ambigous."
+        assert (
+            len(root) == 1
+        ), "There is more than one root of the feature model which makes it ambigous."
 
         self.boolean_fm = self.create_bool_from_fm(root[0])
 
@@ -27,18 +34,24 @@ class FeatureModel:
             constraint_expr = self.create_bool_from_list(constraint_dict)
             expr = boolalg.And(expr, constraint_expr)
         return expr
-    
+
     def create_bool_from_list(self, constraint):
-        assert len(constraint) == 3, "Only two nodes can participate in a link: " + " ".join(constraint)
-        assert constraint[0] in ["req", "exc"], "Not supported connection type: " + constraint[0]
+        assert (
+            len(constraint) == 3
+        ), "Only two nodes can participate in a link: " + " ".join(constraint)
+        assert constraint[0] in ["req", "exc"], (
+            "Not supported connection type: " + constraint[0]
+        )
         x, y = sympy.symbols(constraint[1:])
-        assert x in self.all_symbols or y in self.all_symbols, "Nodes of constraint do not exist: " + " ".join(constraint[1:])
+        assert (
+            x in self.all_symbols or y in self.all_symbols
+        ), "Nodes of constraint do not exist: " + " ".join(constraint[1:])
         if constraint[0] == "req":
             return boolalg.Implies(x, y)
         elif constraint[0] == "exc":
             return boolalg.Not(boolalg.And(x, y))
 
-    def create_bool_from_structure(self, root, parent_name:str):
+    def create_bool_from_structure(self, root, parent_name: str):
         if "abstract" in root["meta"] and root["meta"]["abstract"]:
             parent_symbol = sympy.symbols(parent_name)
             symb = []
@@ -51,15 +64,15 @@ class FeatureModel:
             for s in symb:
                 sub_expr1 = boolalg.Or(sub_expr1, s)
             sub_expr = boolalg.Equivalent(sub_expr1, parent_symbol)
-            
+
             root_node_type = root["meta"]["type"]
             if root_node_type == "alt":
                 sub_expr2 = boolalg.true
                 for i in range(len(symb)):
-                    for j in range(i+1,len(symb)):
+                    for j in range(i + 1, len(symb)):
                         sub_expr2 = boolalg.And(
-                            sub_expr2,
-                            boolalg.Not(boolalg.And(symb[i], symb[j])))
+                            sub_expr2, boolalg.Not(boolalg.And(symb[i], symb[j]))
+                        )
                 sub_expr = boolalg.And(sub_expr, sub_expr2)
             elif root_node_type == "and":
                 pass
@@ -69,13 +82,12 @@ class FeatureModel:
             mandatory = "mandatory" in root["meta"] and root["meta"]["mandatory"]
             for s in symb:
                 if mandatory:
-                    sub_expr = boolalg.And(sub_expr,
-                        boolalg.Equivalent(s, parent_symbol)
+                    sub_expr = boolalg.And(
+                        sub_expr, boolalg.Equivalent(s, parent_symbol)
                     )
                 else:
-                    sub_expr = boolalg.And(
-                        sub_expr, boolalg.Implies(s, parent_symbol)
-                    )
+                    sub_expr = boolalg.And(sub_expr, boolalg.Implies(s, parent_symbol))
+            self.all_symbols.append(sympy.symbols(parent_name))
             return sub_expr
         else:
             s = sympy.symbols(parent_name)
@@ -91,7 +103,7 @@ class FeatureModel:
 
         return True
 
-    def parse_type_code(self, type_code:str) -> specification.Specification:
+    def parse_type_code(self, type_code: str) -> specification.Specification:
         groups = re.split(r"\s|/|(?!\s)-(?!\s)", type_code)
         spec = specification.Specification()
         spec.controller = groups.pop(0)
@@ -110,15 +122,16 @@ class FeatureModel:
         spec.magnet = groups.pop(0) if groups else ""
         return spec
 
-
-    def create_product(self, specification: specification.Specification) -> product.Product:
+    def create_product(
+        self, specification: specification.Specification
+    ) -> product.Product:
         if self.check_specification(specification):
             for group, value in specification.__dataclass_fields__:
-                #assembly_action = self.structure[][group]
-                #prod = Product()
-                #prod.features.add(
+                # assembly_action = self.structure[][group]
+                # prod = Product()
+                # prod.features.add(
                 #    Feature(attr, value, assembly)
-                #)
+                # )
                 pass
 
             # create and return product with features
