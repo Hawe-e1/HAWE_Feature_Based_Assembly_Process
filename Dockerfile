d5efa31ce6edd039a0007feee4715870a8dc5378
Benjamin Baffy <BenjaminBaffy@gmail.com>
2022-08-02 18:09:52 +0200

    boolean feature model fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM conda/miniconda3
 
-RUN conda install python=3.10 sympy fastapi uvicorn 
+RUN conda install -c conda-forge python=3.10 sympy fastapi uvicorn 
+
+COPY ./procgen /assembly-steps-gen/procgen
+
+WORKDIR /assembly-steps-gen
 
 RUN uvicorn procgen.api:app --reload 
--- procgen/data_types.py
+++ procgen/data_types.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
 from typing import Dict, List, Optional, OrderedDict
 
+
 class StructureType(BaseModel):
-    meta : Dict[str, str | bool]
-    nodes : Optional[Dict[str, "StructureType"]]
+    meta: Dict[str, str | bool]
+    nodes: Optional[Dict[str, "StructureType"]]
+
 
 StructureType.update_forward_refs()
 
+
 class OrderConstraintType(BaseModel):
-    expr : List[str]
-    order : List[int]
+    expr: List[str]
+    order: List[int]
+
 
 class FeatureModelType(BaseModel):
     structure: Dict[str, StructureType]
@@ -17,16 +21,20 @@ class FeatureModelType(BaseModel):
     order_constraints: List[OrderConstraintType]
     assembly_steps: Dict[str, List[str]]
 
+
 class SpecificationType(BaseModel):
-    features : Dict[str, str]
+    features: Dict[str, str]
+
 
 class FeatureType(BaseModel):
-    group:str
-    assembly_action:str
+    group: str
+    assembly_action: str
+
 
 class ProductType(BaseModel):
     features: List[FeatureType]
     assembly_order: List[int]
+
 
 class AssemblyProcessType(BaseModel):
     assembly: OrderedDict[str, List[str]]
