f0f46fb52ebaf7e205db3efe1e923f81df53a8b1
Benjamin Baffy <BenjaminBaffy@gmail.com>
2022-08-03 17:19:02 +0200

    cors

--- procgen/api.py
+++ procgen/api.py
@@ -2,8 +2,19 @@ from typing import Dict, List
 import procgen.data_types as data_types
 import procgen.feature_model as feature_model
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
