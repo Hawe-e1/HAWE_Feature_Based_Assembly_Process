12faef9afb7dfede8d3f30c3969d51d2093bfd15
Benjamin Baffy <41485421+BenjaminBaffy@users.noreply.github.com>
2022-09-19 12:45:13 +0200

    Update local_main.py

--- local_main.py
+++ local_main.py
@@ -1,3 +1,9 @@
+"""
+@Authors: A’Qilah Ahmad Dahalan, Jaime Aparicio Estrems, Benjamin Baffy, Sumit Gore, Matteo Mastroguiseppe
+@Date: 2022.09.19
+@Links: https://github.com/Hawe-e1/HAWE_Feature_Based_Assembly_Process
+"""
+
 import uvicorn
 
 if __name__ == "__main__":
